a271712c96031d24ed768c52b1566772f258350d
Jan Brezina <jan.brezina@tul.cz>
2015-03-10 08:59:14 +0100

    Cvika updated.

--- semestralka/R_settings/tenzometr.r
+++ semestralka/R_settings/tenzometr.r
@@ -1,4 +1,20 @@
 # Tenzometr
+#
+# Current model does not make sense: 
+# new = k * l * E
+# old = k * l * (E - c)
+#
+# That means the error is on the load!!
+#
+# TODO: use better model
+# 
+# new = k*l + E
+# old = min(0, k*l -c) +E
+#
+# or rather:
+#
+# old = (k-c)*l + E
+
 load=c(100, 110, 120, 210, 220, 230, 310, 320, 330)
 
 new=round(0.123*load*rnorm(length(load), 1.0, 0.01), digits=2)
